acd72f8ca601237b6d2e12933df80add41cd45e4
”isyu0” <syun.p55@gmail.com>
2020-01-29 04:21:12 +0000

    Admin復活

---
+++ app/Http/Controllers/Admin02/ProfileController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Profile;
+
+class ProfileController extends Controller
+{
+    //追記
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+    
+    //Laravel16課題1:admin/profile/createから送信されてきたフォーム情報をデータベースに保存するようにしましょう
+
+    public function create(Request $request)
+    {
+        // Varidationを行う
+      $this->validate($request, Profile::$rules);
+      
+      $news = new Profile;
+      $form = $request->all();
+      
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      
+      // データベースに保存する
+      $news->fill($form);
+      $news->save();
+      
+      return redirect('admin/profile/create');
+    }
+    
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+    
+    public function update()
+    {
+        return redirect('admin/profile/edit');
+    }
+    
+}
