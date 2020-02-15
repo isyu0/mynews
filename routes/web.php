0d9890cf3ae35b277e530bdf5411c44266917b88
isyu0 <58961106+isyu0@users.noreply.github.com>
2020-02-15 17:15:00 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,9 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
-    Route::get('news', 'Admin\NewsController@index')->middleware('auth'); // 追記
-    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); // 追記
-    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); // 追記
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth'); 
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); 
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); 
     Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
 });
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
@@ -34,4 +34,4 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/', 'NewsController@index');
-Route::get('/profile', 'ProfileController@index')->middleware('auth');
+Route::get('/profile', 'ProfileController@index')->middleware('auth');//追記
